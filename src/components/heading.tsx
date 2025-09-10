3711b3943179173f78f3f2a7368c8d9c149f9876
ntorh <ngcobontombenhle434@gmail.com>
2025-09-10 15:29:16 +0200

    added localstoragefunctions.ts file for nomal function

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -2,14 +2,17 @@
 
 export default function Heading() {
   return (
-    <div style={{
-      backgroundColor: "navy",
-      color: "white",
-      textAlign: "center",
-      paddingTop: "1rem"
-    }}>
-      <h1>Links Vault</h1>
-     
+    <div
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        paddingTop: "1rem",
+      }}
+    >
+      <div id="heading">
+        <h1>Links Vault</h1>
+      </div>
     </div>
   );
 }
